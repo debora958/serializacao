b98c59d455691d2ee33edc3bc33095c2fa60fb96
debora958 <109834177+debora958@users.noreply.github.com>
2022-08-26 18:13:58 -0300

    Add files via upload

---
+++ serializacao.java
@@ -0,0 +1,13 @@
+fileoutoutstream aluno = new fileoutoutstream
+objectoutputstream frequencia = new objectoutputstream
+frequencia.writeobject
+frequencia.close
+
+fileoutoutstream aluno = new fileoutoutstream
+objectinputstream frequencia = new objectinputstream
+objetoserializavel frequencia.readobject
+frequencia.close
+return serializado
+
+
+
